7184376d02a43adeccd5b8b1c2338d9561dc3619
noooon32br <115601277+noooon32br@users.noreply.github.com>
2023-02-05 09:41:33 +0000

    studyruby

--- sample.rb
+++ sample.rb
@@ -5,3 +5,20 @@ puts a
 
 b = 'こんにちは'
 puts b
+
+#オブジェクト(引数1,引数2,引数3)
+#オブジェクト.メソッド
+
+puts 1.to_s()
+
+puts 10.to_s(16)
+
+puts (5 + 1 - 2) * 3
+
+def add(a, b)
+  a + b
+end
+puts add(4, 5)
+
+puts s = 'Hello'
+puts n = 123 * 2
