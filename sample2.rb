bc691d796ae3d57ad6dc78218d6382963c4530f2
noooon32br <115601277+noooon32br@users.noreply.github.com>
2023-02-13 09:19:18 +0000

    2.8.5

--- sample2.rb
+++ sample2.rb
@@ -107,3 +107,19 @@ puts c[0]
 
 #1番目の要素にはTEXT2の内容が入る
 puts c[1]
+
+puts sprintf('%0.3f', 1.2)
+
+#数値を文字列に変換する
+puts 123.to_s
+
+#配列の各要素を連結して1つの文字列にする
+puts [10, 20, 30].join
+
+#*演算子を使って文字列を繰り返す
+puts 'Hi!' * 10
+
+#String.newを使って新しい文字列を作る（あまり使わない）
+puts String.new('Hello')
+
+puts "\u3042\u3044\u3046"
