4f8d2aab613c8ae644f010986823be94cc985fc4
noooon32br <115601277+noooon32br@users.noreply.github.com>
2023-02-13 05:00:27 +0000

    2.8

--- sample2.rb
+++ sample2.rb
@@ -29,3 +29,37 @@ def Hello_world
   'Hello, world'
 end
 
+# %q! !はシングルクォートで囲んだことと同じになる
+puts %!He said, "Don't speak."!
+
+# %Q! !はダブルクオートで囲んだことと同じになる（改行文字や式展開が使える）
+something = "Hello."
+puts %Q!He said, "#{something}"!
+
+# ?を区切り文字として使う
+puts %q?He said, "Don't speak."?
+
+puts "Line 1,
+Line 2"
+
+a = <<TEXT
+これはドキュメントです。
+複数行にわたる長い文字列を作成するのに便利です。
+TEXT
+puts a
+
+# ヒアドキュメントの識別子としてHTMLを使う場合
+<<HTML
+<div>
+  <img src="sample.jpg">
+</div>
+HTML
+
+def some_method
+  <<-TEXT
+これはヒアドキュメントです。
+<<-を使うと最後の識別子をインデントさせることができます。
+  TEXT
+end
+
+puts some_method
