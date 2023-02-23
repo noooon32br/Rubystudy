f2225c20a98469b71ae6f9d0f7e8df0a67b2b039
noooon32br <115601277+noooon32br@users.noreply.github.com>
2023-02-23 06:39:20 +0000

    2.12.6

--- sample3.rb
+++ sample3.rb
@@ -226,3 +226,60 @@ def greet = 'Hello!'
   
 puts greet
 
+
+#if文が値を返すので変数に代入できる
+a = 
+if true
+  '真です'
+else
+  '偽です'
+end
+
+puts a
+
+#メソッドの定義も実は値（シンボル）を返している
+b = def foo; end
+
+puts b
+
+#aとbはどちらも同じ文字列だが、オブジェクトとしては別物
+a = 'hello'
+b = 'hello'
+puts a.object_id
+puts b.object_id
+
+#cにbを代入する。bとcはどちらも同じオブジェクト
+c = b
+puts c.object_id
+
+#メソッドの引数をcを渡す。引数として受け取ったdはb、cと同じオブジェクトを参照している
+def m(d)
+  d.object_id
+end
+
+puts m(c)
+
+#equal?メソッドを使って同じオブジェクトかどうか確認してもいい（trueなら同じオブジェクト）
+puts a.equal?(b)
+puts b.equal?(c)
+
+#b、cは同じオブジェクト、aは異なるオブジェクト
+a = 'hello'
+b = 'hello'
+c = b
+
+#渡された文字列を破壊的に大文字に変換するメソッドを定義する
+def m!(d)
+  d.upcase!
+end
+
+#cにm!メソッドを敵うようする
+m!(c)
+
+#b、cはいずれも大文字になる
+puts b
+puts c
+
+#aは別のオブジェクトなので大文字にならない
+puts a
+
