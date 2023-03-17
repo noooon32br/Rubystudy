1c3f95b294e620e600a5e995ccf1c42b35e041c1
noooon32br <115601277+noooon32br@users.noreply.github.com>
2023-03-17 14:05:27 +0000

    4.6.3

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -8,3 +8,21 @@ end
 
 # puts 0.to_s(16)
 # puts 255.to_s(16)
+
+def to_hex(r, g, b)
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints =[]
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
+end
+
